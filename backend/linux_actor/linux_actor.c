1c80e9f2828b76bac0d92ec94495296b731e4795
Clemens <clemens.jonischkeit@in.tum.de>
2017-05-02 17:10:33 +0200

    cleaned up code of linux_actor
    replaced sprintf with snprintf to prevent stack smashing.
    Implemented delay more nicely

--- backend/linux_actor/linux_actor.c
+++ backend/linux_actor/linux_actor.c
@@ -2,14 +2,15 @@
  * linux_actor.c
  *
  *  Created on: Mar 31, 2017
- *      Author: Dai Yang
+ *      Author: Dai Yang, Clemens Jonischkeit
  *	(C) 2017 Technische Universitaet Muenchen, LRR
  */
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/time.h>
 #include <unistd.h>
+#include <time.h>
+#include <errno.h>
 
 #include "linux_actor.h"
 
@@ -17,17 +18,39 @@ static
 void linux_wait(
 	int delay
 ){
-	struct timeval t;
-	struct timeval dt;
-	if(delay == 0)
-	{
+	struct timespec sleep_time;
+	struct timespec sleep_time_left;
+	int res;
+
+	// do not sleep if you don't have to
+	if (delay <= 0)
 		return;
-	}
-	gettimeofday(&t, NULL);
-	do{
-		sleep(100);
-		gettimeofday(&dt, NULL);
-	}while(dt.tv_sec - t.tv_sec < delay);
+
+	// prepare the struct
+	sleep_time.tv_sec = delay;
+	sleep_time.tv_nsec = 0;
+
+	do {
+		res = nanosleep(&sleep_time, &sleep_time_left);
+		if (res != 0) {
+			switch (errno) {
+			case EFAULT:
+				// there was a problem copying data from user space
+				return;
+			case EINVAL:
+				// nsec was not in [0..999999999]
+				sleep_time.tv_nsec = 0;
+				break;
+			case EINTR:
+				// try to recover from interrupts and possible errors
+				sleep_time.tv_sec = sleep_time_left.tv_sec;
+				sleep_time.tv_nsec = sleep_time_left.tv_nsec;
+				break;
+			default:
+				return;
+			}
+		}
+	} while (res != 0);
 }
 
 void linux_actor_fail(
@@ -50,7 +73,7 @@ void linux_actor_kill(
 ){
 	char buffer [256];
 	linux_wait(delay);
-	sprintf(buffer, "kill %d", pid);
+	snprintf(buffer, sizeof(buffer), "kill %d", pid);
 	system(buffer);
 }
 
@@ -60,6 +83,7 @@ void linux_actor_killall(
 ){
 	char buffer[256];
 	linux_wait(delay);
-	sprintf(buffer, "killall %s", pname);
+	snprintf(buffer, sizeof(buffer), "killall %s", pname);
 	system(buffer);
 }
+
